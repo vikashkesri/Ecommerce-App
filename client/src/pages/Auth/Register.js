f6590019d33e3e9e4b3f292281525286ea14b238
vikashkesri <vk9630623@gmail.com>
2025-10-05 23:23:43 +0530

    Update Register.js

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -26,7 +26,7 @@ const Register = () => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:5000/api/v1/auth/register", 
+        "https://backend-ufwh.onrender.com/api/v1/auth/register", 
         {
           name,
           email,
